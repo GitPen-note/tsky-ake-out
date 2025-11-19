e657a6391c13a1c189dddd3e05299cc3a14c4124
Ray <wsijie2024@163.com>
2025-11-19 23:38:58 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -99,4 +99,18 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    /**
+     *启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable("status") Integer status,long id) {
+        log.info("启用禁用员工账号,参数为：{},{}", status, id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 }
